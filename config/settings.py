a8c17fd660ad9daf4c707fe1c944cebf169595f7
got4v <gota.vasile@gmail.com>
2024-06-26 14:18:53 +0200

    fix: gunicorn project structure

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "contact_manager.apps.ContactManagerConfig",
+    "contact_manager",
 ]
 
 MIDDLEWARE = [
